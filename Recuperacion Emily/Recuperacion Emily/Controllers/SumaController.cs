c1377461980bc53ae7c3ee4d3f02ffe777e13bb9
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-27 11:29:54 -0400

    Se creo controler suma(get)

---
+++ Recuperacion Emily/Recuperacion Emily/Controllers/SumaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Recuperacion_Emily.Controllers
+{
+    
+    public class SumaController : ApiController
+    {
+        [HttpGet]
+        public int suma(int a, int b)
+        {
+            return a + b;
+
+        }
+    }
+}
